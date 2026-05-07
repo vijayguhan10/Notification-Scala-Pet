06e45e5354a8daa066ac17d39c4df29a7ae9d637
vijayguhan10 <vijayguhan10@gmail.com>
2026-05-07 22:28:52 +0530

    Used the vector datastructure for the optimized flow

--- server/notification-server/app/services/EventGenerator.scala
+++ server/notification-server/app/services/EventGenerator.scala
@@ -7,6 +7,10 @@ import java.util.UUID
 import javax.inject.{Inject, Singleton}
 import scala.util.Random
 
+
+
+
+//vector used for optimized datastructure 
 @Singleton
 class EventGenerator @Inject() () {
   private val random = new Random()

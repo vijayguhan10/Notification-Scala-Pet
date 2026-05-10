67434e450ebdae4147c7cc531e67c431e1c1f107
vijayguhan10 <vijayguhan10@gmail.com>
2026-05-10 10:58:45 +0530

    Restructured the folder

---
+++ notification-server/app/models/Db/UserActivityEventRow.scala
@@ -0,0 +1,14 @@
+package models.Db
+import java.time.Instant
+case class UserActivityEventRow(
+    eventId: String,
+    userId: String,
+    sessionId: String,
+    eventType: String,
+    page:String,
+    timestamp: Instant,
+    device:String,
+    browser:String,
+    scrollDepth:Int,
+    localtion:String
+)
--- server/notification-server/project/plugins.sbt
+++
@@ -1,5 +0,0 @@
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.9.5")
-
-addSbtPlugin("org.foundweekends.giter8" % "sbt-giter8-scaffold" % "0.18.0")
-
-addSbtPlugin("io.spray" % "sbt-revolver" % "0.10.0")

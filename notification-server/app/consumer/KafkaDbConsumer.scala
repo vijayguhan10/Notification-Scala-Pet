ea0e890c37834f47680cbb29068d6503bed7bf13
vijayguhan10 <vijayguhan10@gmail.com>
2026-05-12 21:22:26 +0530

    configured the consumer group with the kafka consumer and subscribed with the singletonList for immutability

--- notification-server/app/consumer/KafkaDbConsumer.scala
+++ notification-server/app/consumer/KafkaDbConsumer.scala
@@ -16,3 +16,51 @@ import java.util.concurrent.atomic.AtomicBoolean
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.jdk.CollectionConverters._
+@Singleton
+class KafkaDbConsumer @Inject() (
+    consumerConfig: KafkaConsumerConfig,
+    repository: UserActivityEventRepository,
+    lifecycle: ApplicationLifecycle
+)(implicit ec: ExecutionContext)
+    extends Logging {
+
+  private val running =
+    new AtomicBoolean(true)
+
+  private val props = new Properties()
+
+  props.put(
+    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+    consumerConfig.bootstrapServers
+  )
+  props.put(
+    ConsumerConfig.GROUP_ID_CONFIG,
+    consumerConfig.groupId
+  )
+
+  props.put(
+    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+    classOf[StringDeserializer].getName
+  )
+
+  props.put(
+    ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+    classOf[StringDeserializer].getName
+  )
+
+  props.put(
+    ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,
+    "false"
+  )
+
+  props.put(
+    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+    "earliest"
+  )
+
+  private val consumer = new KafkaConsumer[String, String](props)
+  consumer.subscribe(
+    Collections.singletonList(consumerConfig.topic)
+  )
+
+}

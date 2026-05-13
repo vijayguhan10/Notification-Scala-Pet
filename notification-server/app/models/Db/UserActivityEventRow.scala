d1fffd54ba91070f3ea6464d1b2835478bb6bd85
vijayguhan10 <vijayguhan10@gmail.com>
2026-05-13 11:37:08 +0530

    refctored the code base inside the sc and scala file

--- notification-server/app/models/Db/UserActivityEventRow.scala
+++ notification-server/app/models/Db/UserActivityEventRow.scala
@@ -1,14 +1,14 @@
-package models.Db
+package models.db
 import java.time.Instant
 case class UserActivityEventRow(
     eventId: String,
     userId: String,
     sessionId: String,
     eventType: String,
-    page:String,
+    page: String,
     timestamp: Instant,
-    device:String,
-    browser:String,
-    scrollDepth:Int,
-    localtion:String
+    device: String,
+    browser: String,
+    scrollDepth: Int,
+    location: String
 )

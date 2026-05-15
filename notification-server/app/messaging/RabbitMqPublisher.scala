2ecd3c3c39de624e755d5caf87169686de512302
vijayguhan10 <vijayguhan10@gmail.com>
2026-05-15 09:27:33 +0530

    UPdated with the rabbmitmq publisher service

--- notification-server/app/messaging/RabbitMqPublisher.scala
+++ notification-server/app/messaging/RabbitMqPublisher.scala
@@ -1,1 +1,51 @@
+package messaging
 
+import com.rabbitmq.client._
+import config.RabbitMqConfig
+import models.NotificationMessage
+import play.api.Logging
+import play.api.libs.json.Json
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class RabbitMqPublisher @Inject()(
+    config: RabbitMqConfig
+) extends Logging {
+
+  private val factory =
+    new ConnectionFactory()
+
+  factory.setHost(config.host)
+  factory.setPort(config.port)
+  factory.setUsername(config.username)
+  factory.setPassword(config.password)
+
+  private val connection =
+    factory.newConnection()
+
+  private val channel =
+    connection.createChannel()
+
+  RabbitMqTopology.initialize(
+    channel,
+    config
+  )
+
+  def publish(
+      notification: NotificationMessage
+  ): Unit = {
+
+    val payload =
+      Json.toJson(notification)
+        .toString()
+        .getBytes()
+
+    channel.basicPublish(
+      config.exchange,
+      config.routingKey,
+      MessageProperties.PERSISTENT_TEXT_PLAIN,
+      payload
+    )
+  }
+}

a6d707b32706560d87669127a2b88453cd26de4c
vijayguhan10 <vijayguhan10@gmail.com>
2026-05-15 10:03:13 +0530

    Configured with the Appmodules

--- notification-server/app/modules/AppModule.scala
+++ notification-server/app/modules/AppModule.scala
@@ -2,9 +2,16 @@ package modules
 
 import com.google.inject.AbstractModule
 import consumers.KafkaDbConsumer
+import consumers._
+import dispatchers._
 
 final class AppModule extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[KafkaDbConsumer]).asEagerSingleton()
+     bind(classOf[KafkaNotificationConsumer])
+      .asEagerSingleton()
+
+    bind(classOf[RabbitMqNotificationDispatcher])
+      .asEagerSingleton()
   }
 }
